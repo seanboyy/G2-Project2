5237c97830fd771320cd27a3f8ec236a1c2430a0
wytriam <thomas.m.frick@gmail.com>
2018-04-06 15:02:17 -0400

    added the maze prefabs and Wytriam.STD

---
+++ HorrorMaze/Assets/WytriamSTD/Messenger/Messages.cs
@@ -0,0 +1,9 @@
+﻿public static class Messages
+{
+    public const string INST_ASTEROID = "AsteroidInstantiated";
+    public const string DEST_ASTEROID = "AsteroidDestroyed";
+    public const string INST_SHIP = "ShipInstantiated";
+    public const string DEST_SHIP = "ShipDestroyed";
+    public const string WAVE_CLEAR = "WaveCompleted";
+    public const string GAME_OVER = "GameOver";
+}
