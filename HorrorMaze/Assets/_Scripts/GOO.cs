9670ad315c9e687190022930ce43d46c3fe61f08
Seanboyy <sbamford12@gmail.com>
2018-04-12 16:58:28 -0400

    GOO eyes track player

---
+++ HorrorMaze/Assets/_Scripts/GOO.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOO : MonoBehaviour {
+    public GameObject player;
+    GameObject[] eyes;
+    Quaternion[] startingRotation;
+    bool playerIsCloseEnough = false;
+	// Use this for initialization
+	void Start () {
+        eyes = GameObject.FindGameObjectsWithTag("Eye");
+        startingRotation = new Quaternion[eyes.Length];
+        for(int i = 0; i < startingRotation.Length; ++i)
+        {
+            startingRotation[i] = eyes[i].transform.rotation;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) < 10)
+        {
+            playerIsCloseEnough = true;
+        }
+        else
+        {
+            playerIsCloseEnough = false;
+        }
+        if (playerIsCloseEnough)
+        {
+            foreach (GameObject eye in eyes) {
+                eye.transform.LookAt(player.transform);
+                eye.transform.Rotate(new Vector3(0, -90, 0));
+            }
+        }
+        else
+        {
+            for(int i = 0; i < eyes.Length; ++i)
+            {
+                eyes[i].transform.rotation = startingRotation[i];
+            }
+        }
+	}
+}
