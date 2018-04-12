f25e34b59a5ff647518164ea573cc0142ad90cb6
wytriam <thomas.m.frick@gmail.com>
2018-04-11 22:43:08 -0400

    updated how achievements works

--- HorrorMaze/Assets/_Scripts/Achievements.cs
+++ HorrorMaze/Assets/_Scripts/Achievements.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AchievementsEnum
+{
+    cartographer
+}
+
 public class Achievements : WytriamSTD.Scene_Manager
 {
+    List<AchievementsEnum> achievements;
+
     void Awake()
     {
         Messenger.AddListener(Messages.MAZE_EXPLORED, Cartographer);
+        achievements = new List<AchievementsEnum>();
     }
 
 	// Use this for initialization
@@ -22,6 +30,10 @@ public class Achievements : WytriamSTD.Scene_Manager
 
     void Cartographer()
     {
-        announce("ACHEIVEMENT GET: CARTOGRAPHER\nFully Explore the Maze");
+        if (achievements.Count != 0 && !achievements.Contains(AchievementsEnum.cartographer))
+        {
+            announce("ACHEIVEMENT GET: CARTOGRAPHER\nFully Explore the Maze");
+            achievements.Add(AchievementsEnum.cartographer);
+        }
     }
 }
